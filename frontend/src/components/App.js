00651e81286a86cc80206789615c894882dde956
Botifier <mouath.sai.ensi@gmail.com>
2019-08-07 04:08:54 +0200

    add SearchForm component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Menu from './Menu';
+import SearchForm from './Search';
 
 
 const App = ({ menu }) =>
   <div>
+    <SearchForm />
     <Menu menu={menu} />
   </div>
 
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+import './Search.css';
+
+
+class SearchForm extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleSubmit(event) {
+      if (this.state.value) {
+        alert(this.state.value);
+      }
+      event.preventDefault();
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.value} onChange={this.handleChange} placeholder='Search...'/>
+            <input type="submit" value="Search" />
+        </form>
+      );
+    }
+  }
+
+  export default SearchForm;
