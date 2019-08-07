177aacc9ae3b5ec9a09181727ce5851be6f1faae
Botifier <mouath.sai.ensi@gmail.com>
2019-08-07 03:23:26 +0200

    api: add search

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework.views import status
+
+from .models import MenuItem
+
+
+class MenuViewTest(APITestCase):
+    API_URL = reverse('menu')
+    TEST_DATA = {
+        'names' : ['name1', 'name2'],
+        'prices' : [3.0, 4.0],
+    }
+    TOTAL_ENTRIES = 2**2
+    
+    def setUp(self):
+        super().setUp()
+        self._insert_test_data()
+
+    def _insert_test_data(self):
+        for name in self.TEST_DATA['names']:
+            for price in self.TEST_DATA['prices']:
+                MenuItem.objects.create(
+                    name=name,
+                    price=price,
+                )
+    
+    def test_get_menu(self):
+        response = self.client.get(self.API_URL)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), self.TOTAL_ENTRIES)
+
+    def test_search(self):
+        data = {
+            'search': 'name1'
+        }
+        response = self.client.get(self.API_URL, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), self.TOTAL_ENTRIES / 2)
+        for element in response.data:
+            self.assertEqual(element['name'], 'name1')
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 
 from .models import MenuItem
 from .serializers import MenuItemSerializer
@@ -7,3 +7,5 @@ from .serializers import MenuItemSerializer
 class MenuView(generics.ListAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name',]
