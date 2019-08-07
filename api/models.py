b575ba72c259103846ec6db1f25cde330ad7e13a
Botifier <mouath.sai.ensi@gmail.com>
2019-08-07 02:39:07 +0200

    Add model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=256)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
