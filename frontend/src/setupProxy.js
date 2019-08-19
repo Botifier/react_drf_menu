efc30d5da0fd0c07b801d239bc27bb688692c617
Botifier <mouath.sai.ensi@gmail.com>
2019-08-19 10:38:33 +0200

    add docker-compose
    with 2 services: backend and frontend

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/api/menu', { target: 'http://localhost:8000' }));
+  app.use(proxy('/api/menu', { target: process.env.DOCKERIZED ? 'http://backend:8000/':'http://localhost:8000' }));
 };
